b194578855fb607bbb7653b3df779d43d2da3b3b
Jonathan Podkomorka <jonathanpodko@gmail.com>
2023-04-28 02:31:27 -0400

    Added checks for 3 of the same type in a row

--- main.js
+++ main.js
@@ -6,18 +6,100 @@ const grid = [[1, 1, 1, 0, 0, 0],
               [0, 0, 0, 1, 1, 1],
               [0, 0, 0, 1, 1, 1]]
 
-// Function: Check if more than two of the same digit in a row
-const noTriples = () => {
-    return grid[0][0] === 0 && grid[0][1] === 0 && grid[0][2] === 0 && grid[0][3] === 1 && grid[0][4] === 1 && grid[0][5] === 1
-        && grid[1][0] === 0 && grid[1][1] === 0 && grid[1][2] === 0 && grid[1][3] === 1 && grid[1][4] === 1 && grid[1][5] === 1
-        && grid[2][0] === 0 && grid[2][1] === 0 && grid[2][2] === 0 && grid[2][3] === 1 && grid[2][4] === 1 && grid[2][5] === 1
-        && grid[3][0] === 1 && grid[3][1] === 1 && grid[3][2] === 1 && grid[3][3] === 0 && grid[3][4] === 0 && grid[3][5] === 0
-        && grid[4][0] === 1 && grid[4][1] === 1 && grid[4][2] === 1 && grid[4][3] === 0 && grid[4][4] === 0 && grid[4][5] === 0
-        && grid[5][0] === 1 && grid[5][1] === 1 && grid[5][2] === 1 && grid[5][3] === 0 && grid[5][4] === 0 && grid[5][5] === 0
+
+// Main loop to swap and check if the grid is legal
+let swapType = true
+let count = 0
+while (true) {
+    count++
+
+    // Alternate swapping columns and rows each loop
+    swapType ? swapRandCols() : swapRandRows()
+
+    // Check grid for all requirements
+    if (checkTriples()) {
+        console.log("Found a grid in " + count + " tries.")
+        console.log(grid)
+        break
+    }
+
+    // Prevent looping too long
+    if (count >= 1000 ) {
+        console.log("Reached count " + count + ". Too many tries.")
+        break;
+    }
+
+    const currentGrid = [...grid]
+    console.log(currentGrid)
+
+    swapType = !swapType
 }
 
-// Function: Swap two random rows of the grid              
-const swapRandRows = () => {
+function checkTriples() {
+    if (checkRowsTriples(grid)) {
+        console.log("Triples in rows")
+        return false
+    }
+
+    if (checkColsTriples()) {
+        console.log("Triples in cols")
+        return false
+    }
+
+    return true
+}
+
+function checkColsTriples() {
+    let triples = false
+    columnsAsRows = []
+
+    for (let i = 0; i < grid.length; i++) {
+        column = []
+        grid.forEach((row) => {
+            column.push(row[i])
+            
+        })
+        columnsAsRows.push(column)
+    
+        if (checkRowsTriples(columnsAsRows)) {
+            triples = true
+        }
+    }
+
+    return triples
+}
+
+function checkRowsTriples(inputGrid) {
+    let triples = false
+
+    inputGrid.forEach((row) => {
+        let count = 1
+        let lastNum = row[0]
+    
+        row.forEach((num, i) => {
+            if (i < 1) {
+                return
+            }
+            if (num === lastNum) {
+                count++
+            }
+            else {
+                count = 1
+            }
+
+            if (count === 3) {
+                triples = true
+            }
+
+            lastNum = num         
+        })  
+        
+    })
+
+    return triples
+}
+          
+function swapRandRows() {
 
     // Make array of numbers equal to grid length
     for (var i = 0, rows = []; i < grid.length; i++) {
@@ -38,8 +120,7 @@ const swapRandRows = () => {
     grid[randRow2] = tempRow
 }
 
-// Function: Swap two random columns of the grid 
-const swapRandCols = () => {
+function swapRandCols() {
     // Make array of numbers equal to grid length
     for (var i = 0, cols = []; i < grid.length; i++) cols.push(i)
 
@@ -56,29 +137,4 @@ const swapRandCols = () => {
 
     // Set the second random column equal to the temporary column
     grid.forEach((_, i) => grid[i][randCol2] = tempCol[i])
-}
-
-// Main loop to swap and check if the grid is legal
-let swapType = true
-let count = 0
-while (true) {
-    count++
-
-    // Alternate swapping columns and rows each loop
-    swapType ? swapRandCols() : swapRandRows()
-
-    // Check grid for all requirements
-    if (noTriples()) {
-        console.log("We found a grid in " + count + " tries.")
-        console.log(grid)
-        break
-    }
-
-    // Prevent looping too long
-    if (count >= 1000 ) {
-        console.log("Reached count " + count + ". Too many tries.")
-        break;
-    }
-
-    swapType = !swapType
 }
